e3c84eba8fe134055f7f4d49d13db6ef279dc391
webdeveloper <webdeveloper@Webdeveloper.(none)>
2011-04-25 12:17:52 +0700

    add ignore

---
+++ application/config/facebook.php
@@ -0,0 +1,6 @@
+<?php
+
+$config['facebook_app_id'] 		= '198999666784407';
+$config['facebook_api_secret'] 		= '9684db2a8c25e60d85fc4a6a75d8a14c';
+$config['facebook_default_scope']	= ''; // E.G 'read_stream,birthday,users_events,rsvp_event'
+$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
---
+++ application/config/tweet.php
@@ -0,0 +1,4 @@
+<?php
+
+$config['tweet_consumer_key'] = "SaPLMIy3QQrop4obnh2BA";
+$config['tweet_consumer_secret'] = "TxCeLpB1K7JsJewetdo9h9lFQtKgwusr4dC1vsjHc";
