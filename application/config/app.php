587bdf7338c53305a7f60e06b2847400c3ceed48
Erwin Handoko <bhasunjaya@gmail.com>
2011-04-26 11:01:23 +0700

    delete

--- application/config/app.php
+++
@@ -1,4 +0,0 @@
-<?php
-$config['app_themes'] = 'backends';
-$config['app_title'] = 'Cecille - cute eh?';
-$config['app_upload_path'] = '/uploads/';
--- application/config/tweet.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$config['tweet_consumer_key'] = "SaPLMIy3QQrop4obnh2BA";
-$config['tweet_consumer_secret'] = "TxCeLpB1K7JsJewetdo9h9lFQtKgwusr4dC1vsjHc";
